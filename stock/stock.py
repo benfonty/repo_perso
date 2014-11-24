cd3e3561659f5a192483301466569fa94d263e05
bfonty <bfonty@bouyguestelecom.fr>
2014-11-24 16:00:27 +0100

    ajout de la fonction de contrôle luhn

--- stock/stock.py
+++ stock/stock.py
@@ -39,7 +39,21 @@ references = database.references.find_one()
 
 def controleEtat(etat):
     return etat in references["etats"]
-        
+ 
+ def luhnCheck(card_number):
+    sum = 0
+    num_digits = len(card_number)
+    oddeven = num_digits & 1
+    for count in range(0, num_digits):
+        digit = int(card_number[count])
+        if not (( count & 1 ) ^ oddeven ):
+            digit = digit * 2
+        if digit > 9:
+            digit = digit - 9
+
+        sum = sum + digit
+    return ( (sum % 10) == 0 )
+       
 def controleImei( l ):
     #TODO
     return True
