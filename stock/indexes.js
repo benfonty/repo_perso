370d667a54e0660239a837c09c10d7ddaa304f7d
bfonty <bfonty@bouyguestelecom.fr>
2014-11-26 17:51:26 +0100

    modif paramétrage de la capped collection des usages

--- stock/indexes.js
+++ stock/indexes.js
@@ -1,8 +1,8 @@
 use stock
 db.stock.ensureIndex({psav:1})
 if (db.system.namespaces.find( { name: 'stock.usages' } )) {
-    db.runCommand({"convertToCapped": "usages", size: 10000000});
+    db.runCommand({"convertToCapped": "usages", size: 150000000});
 }
 else {
-    db.createCollection( "usages", { capped: true, size: 10000000 } );
+    db.createCollection( "usages", { capped: true, size: 150000000 } );
 }
