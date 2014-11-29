82353cfa6ae4bdc44d335c0766a070680fb47b16
bfonty <bfonty@bouyguestelecom.fr>
2014-11-29 20:16:36 +0100

    finalisation des essais des aggrégations pour la réappro

--- stock/essai.js
+++ stock/essai.js
@@ -29,3 +29,34 @@ db.usages.aggregate([{
 },{
     $out:"usages_aggreges"
 }])
+db.stock_aggrege.drop()
+db.stock.aggregate([{
+    $match: {
+        etat:{$in:['D','K']}
+    }
+},{
+    $group: {
+        _id:{psav:"$psav",classe:"$reappro"},
+        count:{$sum:1}
+    }
+},{
+    $project:{
+        _id:0,
+        psav:"$_id.psav",
+        stock:{classe:"$_id.classe",count:"$count"}
+    }
+},{
+    $group: {
+        _id:"$psav",
+        stock:{$addToSet:"$stock"}
+    }
+},{
+    $project: {
+        psav:"$_id",
+        _id:0,
+        stock:1
+    }
+},{
+    $out:"stock_aggrege"
+}])
+    
